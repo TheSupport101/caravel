2918c0f6d4654e2f78be983377b48cc0d3a6ebff
TheSupport101 <jpduarte120@gmail.com>
2018-12-31 22:54:00 +0000

    Less colisions!

--- Caravel.py
+++ Caravel.py
@@ -35,6 +35,7 @@ while True:
     # end of the turn.
     command_queue = []
 
+    # The positions occupied by each ship on the next turn.
     next_positions = []
 
     # If we call a get_surrounding_cardinals(), the directions
@@ -42,33 +43,15 @@ while True:
     directions = [Direction.North, Direction.South, Direction.East, Direction.West, Direction.Still]
 
     for ship in me.get_ships():
-        # We check how many halite there is in the current and
-        # surrounding positions and move to the one with the most
-        pos_choices = ship.position.get_surrounding_cardinals() + [ship.position]
-
-        surrounding_hal = [game_map[pos].halite_amount for pos in pos_choices]
-
-        choice_index = surrounding_hal.index(max(surrounding_hal))
-
-        move_choice = directions[choice_index]
-        pos_choice = pos_choices[choice_index]
-
-
+        # 
         if ship.id not in ship_status:
             ship_status[ship.id] = "exploring"
 
-        elif ship.halite_amount >= (constants.MAX_HALITE / 4):
-            logging.info("Ship {} return to base.".format(ship.id))
+        elif ship.halite_amount >= 3 * (constants.MAX_HALITE / 4):
+            logging.info("Ship {} will return to base.".format(ship.id))
             ship_status[ship.id] = "returning"
 
         
-        if ship_status[ship.id] == "exploring":
-            logging.info("Ship {} moving to {}.".format(ship.id, move_choice))
-            if pos_choice not in next_positions:
-                command_queue.append(ship.move(move_choice))
-                next_positions.append(pos_choice)
-            continue
-
         if ship_status[ship.id] == "returning":
             logging.info("Ship {} returning...".format(ship.id))
             if ship.position == me.shipyard.position:
@@ -77,17 +60,39 @@ while True:
                 move = game_map.naive_navigate(ship, me.shipyard.position)
                 logging.info(ship.position.directional_offset(move))
                 if ship.position.directional_offset(move) not in next_positions:
+                    next_positions.append(ship.position.directional_offset(move))
                     command_queue.append(ship.move(move))
+                else:
+                    next_positions.append(ship.position)
                 continue
 
-    """
-            #command_queue.append(
-                #ship.move(
-                    #random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
-            
-        else:
-            command_queue.append(ship.stay_still())
-    """
+        elif ship_status[ship.id] == "exploring":
+            # We check how many halite there is in the current and
+            # surrounding positions and move to the one with the most
+            position_options = ship.position.get_surrounding_cardinals() + [ship.position]
+
+            position_dict = {} # {(0,1): Position(x,y), (etc)}
+            halite_dict = {} # {(0,1): 423, (etc)}
+
+            for n in range(5):
+                position_dict[directions[n]] = position_options[n]
+                halite_dict[directions[n]] = game_map[position_options[n]].halite_amount
+
+            logging.info(position_dict)
+            logging.info(halite_dict)
+        
+            # Finding the key with the maximum value
+            move_choice = max(halite_dict, key=halite_dict.get)
+            position_choice = position_dict[move_choice]
+
+            if position_choice not in next_positions and game_map[position_choice].is_occupied == False:
+                logging.info("Ship {} moving to {}.".format(ship.id, move_choice))
+                command_queue.append(ship.move(move_choice))
+                next_positions.append(position_choice)
+            else:                
+                next_positions.append(ship.position)
+            continue
+
 
     if game.turn_number <= 50 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
         command_queue.append(me.shipyard.spawn())
